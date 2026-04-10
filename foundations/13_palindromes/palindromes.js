fe48f8a9ef0d2b81a7b26f02c77b0e04b7c25309
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 21:55:00 +0300

    Solve exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleanText = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let reversed = "";
+    for (let i = cleanText.length - 1; i >= 0; i--) {
+        reversed += cleanText[i];
+    }
+    return reversed === cleanText;
 };
 
 // Do not edit below this line
