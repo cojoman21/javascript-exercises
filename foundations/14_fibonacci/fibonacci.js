c3c02cce62b51e349fc5e6a147f29e2b68b27917
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 23:03:32 +0300

    Solve exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (position) {
+    if (position < 0) return "OOPS";
+    if (position == 0) return 0;
+    if (position == 1) return 1;
+    let fib = 1;
+    let previous = 0;
+    for (let i = 2; i < position; i++) {
+        previous = fib - previous;
+        fib += previous;
+    }
+    return fib;
+    // 1, 1, 2, 3, 5, 8, 13, 21, 34
+    // fn = fn(1) + p(0) = 1
+    // previous = 1 + 0 = 1
+    // fn = fn(1) + p(1) = 2
+    // p = p(1) + fn(2) = 3
 };
 
 // Do not edit below this line
