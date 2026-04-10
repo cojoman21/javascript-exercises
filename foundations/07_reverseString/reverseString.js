f2f0dedc39a8b733f7609796916a2599ff5a3e5b
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 17:05:24 +0300

    Solve exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let len = word.length;
+    console.log(len);
+    let reversed = "";
+    for (let i = len - 1; i >= 0; i--) {
+        reversed += word.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
