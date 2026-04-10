167c73565e297ecaf208168ec531d1f410c704b0
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 17:53:41 +0300

    Solve exercise 08 with splice()

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...element) {
+    for (let i = 0; i < element.length; i++) {
+        let position = array.indexOf(element[i]);
+        if (position < 0) {
+            continue;
+        }
+        while (position >= 0) {
+            array.splice(position, 1);
+            position = array.indexOf(element[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
