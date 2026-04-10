72c1603eefbc932a33939ecb136c524c1d22aae8
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 20:15:42 +0300

    Solve exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    } else if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    let result = 0;
+    let smaller = x < y ? x : y;
+    let bigger = x < y ? y : x;
+    for (let i = smaller; i <= bigger; i++) {
+        result += i;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
