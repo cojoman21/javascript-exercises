0ad5948fbbf993e3c07f37595c9fe60679dcad67
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-10 21:12:29 +0300

    Solve exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,46 @@
-const add = function() {
-	
+const add = function (x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    let multiplication = array.length < 0 ? 0 : 1;
+    for (let i = 0; i < array.length; i++) {
+        multiplication *= array[i];
+    }
+    return multiplication;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+    return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+    if (x < 0) return Infinity;
+    let factorial = 1;
+    for (let i = x; i > 0; i--) {
+        factorial *= i;
+    }
+    return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
