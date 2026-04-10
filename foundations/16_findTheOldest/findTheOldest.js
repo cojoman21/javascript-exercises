5be3118af284e80ac26d2b5a0a6920b2694011e0
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-11 00:44:16 +0300

    Solve exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let oldestPerson;
+    let previousAge = 0;
+    for (let i = 0; i < array.length; i++) {
+        let currentAge = 0;
+        if ("yearOfDeath" in array[i]) {
+            currentAge = array[i].yearOfDeath - array[i].yearOfBirth;
+        } else {
+            currentAge = new Date().getFullYear() - array[i].yearOfBirth;
+        }
+        let older = currentAge > previousAge ? true : false;
+        if (older) {
+            previousAge = currentAge;
+            oldestPerson = array[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
